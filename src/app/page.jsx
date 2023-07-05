f47a27b616d54caaaf9f27a5bbcea2769442b8b2
Elena <ririteaca@gmail.com>
2023-07-05 20:00:26 +0300

    add Swiper for Phone Brands

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,11 @@
 import Image from 'next/image'
+import Swiper from './components/Swiper'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+		<Swiper />
+		
 		<div className='  bg-cyan-400  rounded-3xl shadow-2xl p-20'>
 			<h1 className=' text-3xl '>
 				Hello reactors team
