11bae1284eaa359ff50f15f6ed4d45b30a858f29
Vlad Cervac <cervacvad32@gmail.com>
2023-07-05 11:07:50 +0200

    starter code

---
+++ src/app/page.jsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+		<div className='  bg-cyan-400  rounded-3xl shadow-2xl p-20'>
+			<h1 className=' text-3xl '>
+				Hello reactors team
+			</h1>
+		</div>
+    </main>
+  )
+}
