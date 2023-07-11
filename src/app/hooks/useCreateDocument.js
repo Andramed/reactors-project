8169f6823923f3ddc416538246e7750f2a09664e
Vlad Cervac <cervacvad32@gmail.com>
2023-07-11 19:26:40 +0200

    remove create database from this hook, add import from other hook

--- src/app/hooks/useCreateDocument.js
+++ src/app/hooks/useCreateDocument.js
@@ -1,31 +1,7 @@
-import { MongoClient } from 'mongodb'
-import uri from '../../../atlas_uri';
-
-
-console.log(uri);
+import { client, connectToDatabase } from './useConectDB';
 
 export const useDataBase = (dbName, colectionName, object) => {
-	const client = new MongoClient(uri);
 	const reviesCollection = client.db(dbName).collection(colectionName) // cream referinta la baza de date dorita daca ea nu va exista va fi creata//
-	const connectToDatabase = async () => {
-	  try {
-		await client.connect()
-		console.log(`Connected to the ${dbName} database 🌍 \nFull connection string: ${uri}`)
-	  } catch (err) {
-		console.error(`Error connecting to the database: ${err}`)
-	  }
-	}
-	// cream documentul
-	// const phone = {
-	//   author: "John Doe",
-	//   coment: "comentariu",
-	//   rating: 4,
-	//   typeOFDeliveri: ['fast', 'slow', 'medium fast']
-	  
-	// }
-	
-	console.log(object);
-	// functia main care se conecteaza la baza de data+ insereaza documentul dorit noua//
 	const main = async () => {
 	  try {
 		await connectToDatabase();
