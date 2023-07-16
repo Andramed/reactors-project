352bc6909d8e74fbbac792596c7ed7e2327fc222
Vlad Cervac <cervacvad32@gmail.com>
2023-07-16 19:31:19 +0200

    added responsive Navbar

--- src/app/components/NavBar/NavBar.jsx
+++ src/app/components/NavBar/NavBar.jsx
@@ -2,28 +2,37 @@ import Link from "next/link";
 
 export const NavBar = () => {
     return(
-        <div className=" w-full h-28 bg-white flex gap-44 items-center">
-            <div className=" pl-14">
-                <h2>Top Phone</h2>
-            </div>
-            <div className="flex gap-7 items-center">
+        <div className=" m-auto w-full h-28 bg-white flex justify-between items-center px-10 gap-2 xs:gap-2 md:justify-around">
+           
+			<img className=' w-[4.8rem]  lg:w-28' src="/logo/logo.jpg" alt="logo"/>
+
+            <div className=" gap-3 justify-between text-sm lg:text-base lg:gap-4 items-center  hidden md:flex ">
                 <Link href={"/"}>Home</Link>
                 <Link href={"/products"}>Products</Link>
                 <Link href={"/brands"}>Brands</Link>
                 <Link href={"/promotions"}>Promotions</Link>
-                <Link href={"/about"}>About Us</Link>
-                <Link href={"/blog"}>Blog</Link>
-                <Link href={"/FAQ"}>FAQ</Link>
-                <Link href={"/contact"}>Contact</Link> 
-                <select value="categories"> 
+                <Link className='hidden w-max lg:flex' href={"/about"}>About Us</Link>
+                <Link className='hidden lg:flex' href={"/blog"}>Blog</Link>
+                <Link className='hidden lg:flex' href={"/FAQ"}>FAQ</Link>
+                <Link  className='hidden lg:flex' href={"/contact"}>Contact</Link>
+                
+            </div>
+			<select  className=' text-sm hidden xs:flex lg:text-base '> 
                     <option value="phone">All Categories</option>
                     <option value="smartphone">Smart Phone</option>
                     <option value="classicphone">Classic Phone</option>
-                </select>
-               <div className=" gap-2">
-                <input type="text" placeholder="Brand..."/>
-                <button className=" w-[50px] h-[50px] p-3 rounded bg-btn-color"><img src="/imgNavBar/searchNav.svg" alt="search" /></button>
-               </div>
+            </select>
+			<div className=' md:hidden  '>
+				<button>
+				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 17" strokeWidth={1.5} stroke="currentColor" className="w-6 h-10">
+  					<path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+				</svg>
+
+				</button>
+			</div>
+			<div className=" gap-2 flex w-full  md:w-full lg:text-sm   ">
+                <input className=' border w-full md:w-full ' type="text" placeholder="Brand..."/>
+                <button className=" w-14 h-8 flex items-center justify-center  rounded bg-btn-color"><img className=' w-1/2 h-1/2' src="/imgNavBar/searchNav.svg" alt="search" /></button>
             </div>
         </div>
     )
