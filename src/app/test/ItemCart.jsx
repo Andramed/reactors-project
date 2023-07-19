95ee95b642fe05ff21d80c6dabb95e67a2b22880
Vlad Cervac <cervacvad32@gmail.com>
2023-07-19 23:53:37 +0200

    create test page for test feaatures + add component Item Cart

---
+++ src/app/test/ItemCart.jsx
@@ -0,0 +1,24 @@
+import React, { useRef } from 'react'
+import { useAddItemToCart } from '../hooks/useAddItemToCart';
+
+
+export const ItemCart = (props) => {
+	
+	const {product,  getProduct, onClick} = props;
+	console.log(product.type);
+	
+	const parentRef = useRef(0);
+  return (
+	<>
+	
+		<div className=' flex justify-center m-10'>
+				<div onClick={() => getProduct(product)} id={product._id}  ref={parentRef} className=' w-[500px] h-10 bg-red-100 rounded-sm  flex flex-col items-center justify-center p-24' >
+					{product.type} 
+					{product.brand}
+					<h4>{product.model}</h4>
+				</div>
+				<button onClick={()=>onClick(parentRef.current)} >add to cart</button>
+		</div> 
+	</>
+  )
+}
