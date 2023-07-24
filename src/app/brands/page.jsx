076d6bc72bc38eb13065be3e1f296a793f488077
Vlad Cervac <cervacvad32@gmail.com>
2023-07-24 13:44:18 +0200

    delete error

--- src/app/brands/page.jsx
+++ src/app/brands/page.jsx
@@ -1,10 +1,10 @@
 'use client'
 
-import { useProductContext } from '../hooks/useContext';
+
 
 const Page = () => {
 
-    const { cartItem, setCartItem } = useProductContext();
+   
 
     
     console.log(cartItem + 'from page Brands');
