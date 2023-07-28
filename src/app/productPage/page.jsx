b8a231ec1605f4510a780e2a3d26dbbd45a8ce98
MPlugaru89 <mihaiplugaru89@gmail.com>
2023-07-28 21:03:12 +0200

    slider done

--- src/app/productPage/page.jsx
+++ src/app/productPage/page.jsx
@@ -9,38 +9,52 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 
-
 function ProductPage() {
   const parentRef = useRef(0);
   const [product, setProduct] = useState();
   const { getIdItem } = useAddItemToCart();
+  const [memory, setMemory] = useState('');
+  const [ram, setRam] = useState ('');
+  const [memoryPriceDiff, setMemoryPriceDiff] = useState(0);
 
- 
 
+  const handleOnChangeMemory = (e) => {
+    setMemory(e.target.value)
+    const selectedMemoryOption = e.target.value;
+    const priceDiff = selectedMemoryOption === '' ? 0 : 500;
+    setMemoryPriceDiff(priceDiff);
+  
+  }
+
+  const updateProductPrice = () => {
+    const updatedPrice = product.price + memoryPriceDiff;
+    return updatedPrice;
+  };
+  
+  const handleOnChangeRAM = (e) => {
+    setRam(e.target.value)
+  }
   const [mainPicture, setMainPicture] = useState(0);
 
-		useEffect(() => {
-			const itemLocal = sessionStorage.getItem('item');
-			setProduct(JSON.parse(itemLocal));
-		}, [])
-		
-		if(product){
-			console.log(product, 'pageproduct');
+  useEffect(() => {
+    const itemLocal = sessionStorage.getItem("item");
+    setProduct(JSON.parse(itemLocal));
+  }, []);
 
-		}
-		
-		if(product) {
-			console.log(Object.entries(product.color_image)[0][1]);
-		}
-  if (!product ) { 
-	return <h1>Loading</h1>;
+  if (product) {
+    console.log(product, "pageproduct");
+  }
+
+  if (product) {
+    console.log(Object.entries(product.color_image)[0][1]);
+  }
+  if (!product) {
+    return <h1>Loading</h1>;
   } else {
+    
     return (
-		
-		// componenta IMAGE, 
-		// component  Propreties
-      <div  className="max-w-7xl mx-auto px-4 py-8 sm:px-6 lg:px-8 product">
-		
+      
+      <div className="max-w-7xl mx-auto px-4 py-8 sm:px-6 lg:px-8 product">
         <span className="self-start ml-10">
           <button className="text-gray-300 hover:text-red-500">
             <HeartIcon className="w-8 h-8" />
@@ -50,17 +64,18 @@ function ProductPage() {
           <div className="md:w-1/2 lg:w-1/3 flex flex-col items-center mb-6">
             <div className="w-auto h-56 sm:h-72 lg:h-96 overflow-hidden">
               <img
-                src={Object.entries(product.color_image)[0][1][mainPicture]} // aici trebui un filtru care se privesasca daca este array // sau obiect 
-				// sau string
-				// src={product.color_image.prima culoare[mainPicture]} metoda pentru iterare a obiectelor
+                src={Object.entries(product.color_image)[0][1][mainPicture]}
                 className="object-contain w-full h-full"
                 alt="mainPicture"
               />
             </div>
             <div className="mt-6 flex space-x-2">
-              {Object.entries(product.color_image)[0][1] // prima care o gaseste color_iamge  key, value
-                .slice(Object.entries(product.color_image)[0][1], Object.entries(product.color_image)[0][1].length) // slice(0, 4) 
-				// prima vlaoare la slice arr[0]
+              {Object.entries(product.color_image)[0][1]
+                .slice(
+                  Object.entries(product.color_image)[0][1],
+                  Object.entries(product.color_image)[0][1].length
+                )
+
                 .map((color_image, index) => (
                   <button
                     key={index}
@@ -94,7 +109,7 @@ function ProductPage() {
                 >
                   <span className="w-1.5 h-1.5 rounded-full bg-yellow-600" />
                   <span className="text-sm font-semibold">
-                    Memory RAM: {product.ram[0]}
+                    Memory RAM: {ram ||product.ram[0]}
                   </span>
                 </li>
               </ul>
@@ -106,7 +121,7 @@ function ProductPage() {
                 >
                   <span className="w-1.5 h-1.5 rounded-full bg-yellow-600" />
                   <span className="text-sm font-semibold">
-                    Internal memory: {product.memory[0]}
+                    Internal memory: {memory || product.memory[0]}
                   </span>
                 </li>
               </ul>
@@ -124,8 +139,8 @@ function ProductPage() {
                   <label htmlFor="quantity" className="text-sm">
                     Quantity:
                   </label>
-                  <input 
-                  onChange= {(e) => handleCounter(e)}
+                  <input
+                    onChange={(e) => handleCounter(e)}
                     type="number"
                     defaultValue="1"
                     min="1"
@@ -141,7 +156,6 @@ function ProductPage() {
                     id="color"
                     className="form-select py-1 pl-2 w-full max-w-xs rounded border-2 border-gray-300 bg-gray-100 text-gray-500 focus:border-yellow-600 focus:ring-0"
                   >
-                    <option value="">Select color</option>
                     {Object.keys(product.color_image).map((colorKey, index) => (
                       <option key={index} value={colorKey}>
                         {colorKey}
@@ -151,14 +165,29 @@ function ProductPage() {
                 </div>
                 <div className="flex-1 flex items-center space-x-2">
                   <label htmlFor="size" className="text-sm">
-                    Memory:
+                    Memory RAM:
                   </label>
-                  <select
+                  <select onChange={handleOnChangeRAM}
                     name="size"
                     id="size"
                     className="form-select py-1 pl-2 w-full max-w-xs rounded border-2 border-gray-300 bg-gray-100 text-gray-500 focus:border-yellow-600 focus:ring-0"
                   >
-                    <option value="">Select memory</option>
+                    {product.ram.map((memoryOption, index) => (
+                      <option key={index} value={memoryOption}>
+                        {memoryOption}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+                <div className="flex-1 flex items-center space-x-2">
+                  <label htmlFor="size" className="text-sm">
+                    Memory:
+                  </label>
+                  <select onChange={handleOnChangeMemory}
+                    name="size"
+                    id="size"
+                    className="form-select py-1 pl-2 w-full max-w-xs rounded border-2 border-gray-300 bg-gray-100 text-gray-500 focus:border-yellow-600 focus:ring-0"
+                  >
                     {product.memory.map((memoryOption, index) => (
                       <option key={index} value={memoryOption}>
                         {memoryOption}
@@ -166,22 +195,20 @@ function ProductPage() {
                     ))}
                   </select>
                 </div>
+               
                 <div ref={parentRef} id={product._id} className="flex-1">
                   <button
-
                     type="button"
                     className="w-full py-2 px-4 inline-flex items-center justify-center rounded-md bg-yellow-500 text-base text-white font-semibold uppercase whitespace-nowrap hover:bg-yellow-600"
-                    onClick={() => getIdItem(parentRef.current)}
+                    onClick={() => getIdItem(parentRef.current, updateProductPrice())}
                   >
                     <ShoppingBagIcon className="w-6 h-6 mr-2" />
                     Add to Cart
                   </button>
                 </div>
               </div>
-              {/* pret */}
-              <span className="text-3xl font-bold">
-                {product.price} lei
-              </span>
+             
+              <span className="text-3xl font-bold">{ updateProductPrice()} lei</span>
             </div>
           </div>
         </div>
