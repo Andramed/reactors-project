7bcf85f23059b54d4f7ecdba75c412678ae6c734
Cervac Ivanka <cervac.ivanka@gmail.com>
2023-07-29 22:01:48 -0800

    add PayForm

--- src/app/components/Footer/PayWith.jsx
+++ src/app/components/Footer/PayWith.jsx
@@ -1,10 +1,29 @@
+'use clinet'
+import { useState } from "react";
+
 export default function PayWith () {
+    const [showFormPay, setshowFormPay] = useState(false)
+
+    const openFormPay = () => {
+        setshowFormPay(true)
+        return console.log('am facut click');
+        
+    }
+
+    const processPayment = () => {
+        alert('Plata a fost efectuată cu succes!');
+      };
+
+
     return(
+       <>
        <div className=" flex flex-col gap-2 "> 
             <h5 className=" text-sm sm:text-lg lg:text-xl">Pay With</h5>
             < div className=" sm:flex gap-2 ">
                 <div className=" flex justify-center items-center gap-2 pt-2 xs:pt-0 ">
+                    <button onClick={openFormPay}>
                     <img src="/imgFooter/PayPal.svg" alt="paypal" />
+                    </button>
                     <img src="/imgFooter/GooglePay.svg" alt="gpay" />
                     <img src="/imgFooter/Amex.svg" alt="amex" />
                     <img src="/imgFooter/Bancontact.svg" alt="bnc" />
@@ -16,5 +35,18 @@ export default function PayWith () {
                 </div>
             </div>
        </div>
+       <div>
+       {showFormPay && (
+        <div>
+          <h2>Payment Form</h2>
+          <p>Introduceți detaliile de plată:</p>
+          <input type="text" placeholder="Nume pe card" /><br />
+          <input type="text" placeholder="Număr card" /><br />
+          <input type="text" placeholder="Data expirării" /><br />
+          <button onClick={processPayment}>Pay</button>
+        </div>
+      )}
+       </div>
+       </>
     )
 }
