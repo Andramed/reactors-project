e9dbdef083bb92364d4087ca00dd7449f8e10249
MPlugaru89 <mihaiplugaru89@gmail.com>
2023-08-02 19:22:29 +0200

    some changes

--- src/app/productPage/RelatedProducts.jsx
+++ src/app/productPage/RelatedProducts.jsx
@@ -3,12 +3,14 @@ import axios from "axios";
 import useSWR from "swr";
 import { Swiper, SwiperSlide } from "swiper/react";
 import  { Navigation } from "swiper/modules";
-
+import useGetProduct from "../hooks/useGetProduct";
+import MayAlsoLike from "./MayAlsoLike";
 
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar"
+
 
 function RelatedProducts({ product }) {
   const fetcher = async (url) => await axios.get(url).then((res) => res.data);
@@ -18,7 +20,7 @@ function RelatedProducts({ product }) {
     `/api/getRandomProducts?price=${product.price}&limit=${MAXProducts}`,
     fetcher
   );
-  
+  const {getProduct} = useGetProduct()
 
   if (!data) {
     return <h1>Loading...</h1>;
@@ -40,26 +42,13 @@ function RelatedProducts({ product }) {
           modules={[Navigation]}
           pagination = {{clickable : true}}
           scrollbar={{ draggable: true }}
-          loop = {true}
+          // loop = {true}
         >
           {data.map((relatedProduct, index) => (
             <SwiperSlide key={index} >
               
-              <div key={index} className="bg-gray-100 p-4 rounded-md">
-                <img
-                  src={Object.entries(relatedProduct.color_image)[0][1][0]}
-                  className="object-contain w-32 h-32 mx-auto"
-                  alt={`Related Product ${index}`}
-                />
-
-                <h3 className="text-center text-lg font-semibold mt-2">
-                  {relatedProduct.brand} {relatedProduct.model}
-                </h3>
-                <span className=" text-gray-500 ">
-                  {relatedProduct.price} lei
-                </span>
-              </div>
-              
+              <MayAlsoLike relatedProduct={relatedProduct} getProduct={getProduct} index={index} />
+             
             </SwiperSlide>
           ))}
         </Swiper>
